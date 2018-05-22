995a748c50bd636bcbcff5a62b931fabc7c12f98
kvega <kevin.n.vega@gmail.com>
2018-05-22 12:18:29 -0700

    Change input sanitation

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,7 +1,6 @@
 # "Database code" for the DB Forum.
 
-import psycopg2
-import bleach
+import psycopg2, bleach
 
 DBNAME = "forum"
 
@@ -12,14 +11,13 @@ def get_posts():
   c.execute("select content, time from posts order by time desc")
   posts = c.fetchall()
   db.close()
-  return posts
+  return bleach.clean(posts)
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
-  content = bleach.clean(content)
-  c.execute("insert into posts values (%s)" , (content,))
+  c.execute("insert into posts values (%s)" , (bleach.clean(content),))
   db.commit()
   db.close()
 
